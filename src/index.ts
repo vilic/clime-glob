aa00589b65927db2635240048381c5d271c73675
vilic <i@vilic.info>
2017-11-08 18:56:53 +0800

    Upgrade everything

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,22 @@
-import { Context } from 'clime';
+import {CastingContext} from 'clime';
 import * as globby from 'globby';
 
 export class Matches extends Array<string> {
-    private constructor(...args: any[]) {
-        super(...args);
-    }
+  private constructor(...args: any[]) {
+    super(...args);
+  }
 
-    static async cast(globsStr: string, context: Context): Promise<Matches> {
-        let globs = globsStr
-            .split(',')
-            .map(glob => glob.trim())
-            .filter(glob => !!glob);
+  static async cast(
+    globsStr: string,
+    context: CastingContext<Matches>,
+  ): Promise<Matches> {
+    let globs = globsStr
+      .split(',')
+      .map(glob => glob.trim())
+      .filter(glob => !!glob);
 
-        let paths = await globby(globs, {
-            cwd: context.cwd
-        });
+    let paths = await globby(globs, {cwd: context.cwd});
 
-        return new this(...paths);
-    }
+    return new this(...paths);
+  }
 }
